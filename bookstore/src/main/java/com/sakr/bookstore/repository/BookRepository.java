ef992783ec22d6681d72aafdc26836834f2520f1
Abdelrahman Sakr <abdelrahman.mansour04@eng-st.cu.edu.eg>
2026-07-12 14:11:22 +0300

    Added H2 configuration in applications.properties

--- bookstore/src/main/java/com/sakr/bookstore/repository/BookRepository.java
+++ bookstore/src/main/java/com/sakr/bookstore/repository/BookRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sakr.bookstore.model.Book;
-import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long>{
     Optional<Book> findByIsbn(String isbn);
